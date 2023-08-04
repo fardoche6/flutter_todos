4f524fe21e75283c5f2e7de38ef0fe17656e297f
Alexandre Archambault <alexandre.archambault@hotmail.com>
2023-08-04 19:14:59 -0400

    Since flutter update, put correction.

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -3,15 +3,19 @@ import 'package:flutter/material.dart';
 class FlutterTodosTheme {
   static ThemeData get light {
     return ThemeData(
+      // primaryColor: Colors.white,
       appBarTheme: const AppBarTheme(
         color: Color(0xFF13B9FF),
       ),
       colorScheme: ColorScheme.fromSwatch(
+        // backgroundColor: const Color.fromARGB(255, 16, 24, 40),
         accentColor: const Color(0xFF13B9FF),
       ),
       snackBarTheme: const SnackBarThemeData(
         behavior: SnackBarBehavior.floating,
       ),
+      floatingActionButtonTheme:
+          const FloatingActionButtonThemeData(shape: CircleBorder()),
     );
   }
 
@@ -27,6 +31,8 @@ class FlutterTodosTheme {
       snackBarTheme: const SnackBarThemeData(
         behavior: SnackBarBehavior.floating,
       ),
+      floatingActionButtonTheme:
+          const FloatingActionButtonThemeData(shape: CircleBorder()),
     );
   }
 }
